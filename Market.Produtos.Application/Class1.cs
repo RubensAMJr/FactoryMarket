931cdad3428f22be0415624991e94ac74c73fa72
Carlos Oliveira <Caik_oliveirapires@hotmail.com>
2020-09-22 14:43:38 -0300

    primeiro commit pastas and projects

---
+++ Market.Produtos.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Market.Produtos.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Market.Produtos.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Market.Produtos.Domain
+{
+    public class Class1
+    {
+    }
+}
